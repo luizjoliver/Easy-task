d8fa4489c671e14594898bc5f9d1d08030fd5226
luizjoliver <luizfellipejoliver14@gmail.com>
2025-03-24 22:38:32 -0300

    Adicionando persistencia no projeto com o localStorage

--- src/app/components/tasks/tasks.service.ts
+++ src/app/components/tasks/tasks.service.ts
@@ -9,6 +9,15 @@ export class TasksService {
   private tasks = dummyTasks
 
 
+  constructor(){
+    const tasks = localStorage.getItem('tasks') as string
+
+    if(tasks){
+      this.tasks = JSON.parse(tasks)
+    }
+  }
+
+
   getUserTasks(userId:string){
 
     return this.tasks.filter((task) => task.userId === userId)
@@ -22,10 +31,16 @@ export class TasksService {
       dueDate:task.dueDate,
       userId:userId
     })
-
+    this.saveTasks()
   }
 
   removeTask(id:string){
     this.tasks = this.tasks.filter((task) => task.id !== id)
+    this.saveTasks()
+  }
+
+  private saveTasks (){
+    localStorage.setItem('tasks',JSON.stringify(this.tasks))
+
   }
 }
